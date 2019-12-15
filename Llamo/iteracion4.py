efa3a290e9b2a01eefe59cd62d2ee0daf1261977
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-15 13:07:51 -0500

    ITERACION4

---
+++ Llamo/iteracion4.py
@@ -0,0 +1,17 @@
+#PROGRAMA PARA MOSTRAR EN PATALLA LOS DECODIFICADORES "NOS VEMOS EN LA FIESTA CACHIMBO"
+msg=""
+import os
+
+msg=str(os.sys.argv[1])
+
+for letra in msg:
+    if (letra=="J"):
+        print("NOS")
+    if(letra=="H"):
+        print("VEMOS EN LA")
+    if(letra=="O"):
+        print("FIESTA")
+    if(letra=="N"):
+        print("CACHIMBO")
+    #fin_if
+#fin_for
