45075158d41ea2fd4700655892c2721aa4758381
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-15 13:34:58 -0500

    BUCLE ITERACION2

---
+++ Catter/iteracion2.py
@@ -0,0 +1,18 @@
+#PROGRAMA PARA MOSTRAR EN PATALLA LOS DECODIFICADORES "MI NUMERO DE CELULAR ES 987654321"
+msg=""
+import os
+
+msg=str(os.sys.argv[1])
+
+for letra in msg:
+    if (letra=="T"):
+        print("MI NUMERO")
+    if(letra=="S"):
+        print("DE CELULAR")
+    if(letra=="R"):
+        print("ES")
+    if(letra=="M"):
+        print("987654321")
+    #fin_if
+#fin_for
+
