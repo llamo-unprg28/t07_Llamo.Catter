31621244920c20308817f2522b447e3871c947a0
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-15 13:17:38 -0500

    BUCLE WHILE5

---
+++ Catter/while5.py
@@ -0,0 +1,9 @@
+#LAS PERSONAS DE APELLIDO CATTER TIENEN UN CEVICHE GRATIS
+#MOSTRAR EN PANTALLA TIENE CEVICHE GRATIS
+apellido=""
+apellido_invalido=(apellido!="CATTER")
+while(apellido_invalido):
+    apellido=input("ingresar apellido:")
+    apellido_invalido=(apellido!="CATTER")
+#fin_while
+print("tiene ceviche gratis")
