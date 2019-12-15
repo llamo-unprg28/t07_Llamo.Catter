a7c760c27e7b2e97c12eb08423475308875b1fb8
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-15 13:48:15 -0500

    LOTE.BAT

---
+++ lote.bat
@@ -0,0 +1,44 @@
+python Llamo/iteracion1.py "X X Z 1"
+python Llamo/iteracion1.py "Y Y Z 1"
+python Llamo/iteracion1.py "X Y 1 1"
+
+python Llamo/iteracion2.py "0 1 2"
+python Llamo/iteracion2.py "2 1 1"
+python Llamo/iteracion2.py "1 0 2"
+
+python Llamo/iteracion3.py "= ! ¿"
+python Llamo/iteracion3.py "¿ ! ¿"
+python Llamo/iteracion3.py "! ! !"
+
+python Llamo/iteracion4.py "J H O N "
+python Llamo/iteracion4.py "N H O N "
+python Llamo/iteracion4.py "N N N N "
+
+python Llamo/iteracion5.py "* * * "
+python Llamo/iteracion5.py "$ $ $ "
+python Llamo/iteracion5.py "/ / / "
+
+
+python Catter/iteracion1.py "A B C "
+python Catter/iteracion1.py "A A A "
+python Catter/iteracion1.py "C C C "
+
+python Catter/iteracion2.py "T S R M "
+python Catter/iteracion2.py "T S M M "
+python Catter/iteracion2.py "T S S S "
+
+python Catter/iteracion3.py "L E P "
+python Catter/iteracion3.py "P E P "
+python Catter/iteracion3.py "E E E "
+
+python Catter/iteracion4.py "1 2 3 "
+python Catter/iteracion4.py "2 2 2 "
+python Catter/iteracion4.py "3 3 3 "
+
+python Catter/iteracion5.py "? = ! "
+python Catter/iteracion5.py "? ? ? "
+python Catter/iteracion5.py "! ! ! "
+
+
+
+
